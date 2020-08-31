ce2eb61943a70ab0b8cf98759b8e75ffd6b91615
lukaszbrzozowski <lukaszbrzozowski1998@gmail.com>
2020-08-31 15:08:00 +0200

    Struc2Vec - without OPT1-3

--- examples/exampleStruct2Vec.py
+++ examples/exampleStruct2Vec.py
@@ -1,9 +1,13 @@
 import numpy as np
 import networkx as nx
 from engthesis.embeddings.node.struc2vec import Struct2Vec
+import matplotlib.pyplot as plt
 
-bg = nx.barbell_graph(10, 1)
+bg = nx.barbell_graph(80, 30)
+nx.draw(bg, with_labels=True)
+plt.show()
+s2v = Struct2Vec(bg, d=2, gamma=1, T=20)
 
-s2v = Struct2Vec(bg)
-
-s2v.generate_similarity_matrices()
+Z = s2v.embed()
+plt.scatter(Z[:, 0], Z[:, 1])
+plt.show()
