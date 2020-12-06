e1bb5844dada7ca4bd554e44c7eeb5e553dcbb0a
lukaszbrzozowski <lukaszbrzozowski1998@gmail.com>
2020-12-06 18:42:38 +0100

    new LINE

--- relegy/embeddings/LINE/__init__.py
+++ relegy/embeddings/LINE/__init__.py
@@ -2,14 +2,14 @@ import numpy as np
 import networkx as nx
 from networkx import Graph
 from relegy.__base import Model
+import tensorflow as tf
 
 construct_verification = {"graph": [(lambda x: type(x) == Graph, "'graph' must be a networkx graph")]}
 
 init_verification = {"d": [(lambda d: d > 0, "d has to be greater than 0.")]}
 
-init_model_verification = {"batch_size": [(lambda x: x > 0, "batch_size must be greater than 0.")],
-                           "lmbd1": [(lambda x: x > 0, "lmbd1 must be greater than 0.")],
-                           "lmbd2": [(lambda x: x > 0, "lmbd2 must be greater than 0.")],
+init_model_verification = {"lmbd1": [(lambda x: x >= 0, "lmbd1 must be non-negative.")],
+                           "lmbd2": [(lambda x: x >= 0, "lmbd2 must be non-negative.")],
                            "lr1": [(lambda x: x > 0, "lr1 must be greater than 0.")],
                            "lr2": [(lambda x: x > 0, "lr2 must be greater than 0.")]}
 
@@ -25,7 +25,6 @@ class LINE(Model):
     ' J. Tang, M. Qu, M. Wang, M. Zhang, J. Yan, and Q. Mei. Line: Large-scale information network embedding. In
     WWW, 2015.'
     """
-
     @Model._verify_parameters(rules_dict=construct_verification)
     def __init__(self,
                  graph: Graph):
@@ -38,80 +37,87 @@ class LINE(Model):
         self.__d = None
         self.__lr1 = None
         self.__lr2 = None
-        self.__batch_size = None
         self.__lmbd1 = None
         self.__lmbd2 = None
+        self.__opt1 = None
+        self.__opt2 = None
         self.__A = None
         self.__U1 = None
         self.__U2 = None
-        self.__Z = None
-        self.__E = None
-        self.__o1 = None
-        self.__o2 = None
-        self.__Frob = None
-        self.__grad1 = None
-        self.__grad2 = None
+        self.__N = None
 
     @Model._init_in_init_model_fit
     @Model._verify_parameters(rules_dict=init_verification)
     def initialize(self,
-                   d: int = 2,
-                   **kwargs):
+                   d=2):
         """
         LINE - initialize (step II) \n
-        Generation of loss functions and input matrices.
+        Generation of adjacency matrix and input matrices.
         @param d: The embedding dimension.
-        @param kwargs: Two input matrices 'U1' and 'U2' both of dimension Nxd may be passed here. They are generated
-        from the uniform distribution on [0.0, 1.0) otherwise.
-
         """
-        graph = self.get_graph()
+        self.__N = len(self.get_graph().nodes)
         self.__d = d
-        self.__E = len(graph.edges)
-        self.__U1 = kwargs["U1"] if "U1" in kwargs else np.random.random((len(graph.nodes), self.__d))
-        self.__U2 = kwargs["U2"] if "U2" in kwargs else np.random.random((len(graph.nodes), self.__d))
-        self.__A = nx.to_numpy_array(graph, nodelist=np.arange(len(graph.nodes)))
-        self.__Frob = lambda U: np.linalg.norm(U)
-        p1 = lambda x, y: 1 / (1 + np.exp(-np.dot(x, y)))
-        p2 = lambda x, y: np.exp(np.dot(x, y)) / (np.sum(self.__U2 @ y))
-        self.__o1 = lambda G: -sum([self.__A[i, j] * np.log(p1(self.__U1[i, :], self.__U1[j, :])) for (i, j) in G.edges])
-        self.__o2 = lambda G: -sum([self.__A[i, j] * np.log(p2(self.__U2[i, :], self.__U2[j, :])) for (i, j) in G.edges])
-        self.__grad1 = lambda i, j: self.__A[i, j] * (-self.__U1[j, :]) * \
-                                    np.exp(-np.dot(self.__U1[i, :], self.__U1[j, :])) / \
-                                    (1 + np.exp(-np.dot(self.__U1[i, :], self.__U1[j, :]))) ** 2
-        self.__grad2 = lambda i, j: self.__A[i, j] * (
-                    self.__U2[j, :] * np.exp(np.dot(self.__U2[i, :], self.__U2[j, :])) * np.sum(
-                self.__U2 @ self.__U2[j, :]) - np.exp(np.dot(self.__U2[i, :], self.__U2[j, :])) * self.__U2[j, :]) / (
-                                 np.sum(self.__U2 @ self.__U2[j, :])) ** 2
+        self.__A = tf.convert_to_tensor(nx.to_numpy_array(self.get_graph(), nodelist=np.arange(self.__N)), dtype="float32")
+        self.__U1 = tf.Variable(tf.random.uniform([self.__N, d]))
+        self.__U2 = tf.Variable(tf.random.uniform([self.__N, d]))
+
+    def __get_loss1(self):
+        return -tf.reduce_sum(tf.multiply(self.__A,
+                                          tf.math.log(tf.divide(1,
+                                                                1+tf.math.exp(tf.matmul(-self.__U1,
+                                                                                        tf.transpose(self.__U2))))))) + tf.reduce_sum(self.__lmbd1 * (tf.abs(self.__U1)+tf.abs(self.__U2)))
+
+    def __get_loss2(self):
+        d_temp = tf.reduce_sum(self.__A, axis=1)
+        d = tf.tile(tf.reshape(d_temp, [self.__N, 1]), tf.constant([1, self.__N]))
+        mlog = tf.math.log(tf.divide(self.__A, d))
+        return -tf.reduce_sum(tf.math.multiply_no_nan(mlog, self.__A)) + tf.reduce_sum(self.__lmbd2 * (tf.abs(self.__U1)+tf.abs(self.__U2)))
+
+    def __get_gradients1(self):
+        with tf.GradientTape() as tape:
+            tape.watch([self.__U1, self.__U2])
+            L = self.__get_loss1()
+        g = tape.gradient(L, [self.__U1, self.__U2])
+        return g
+
+    def __get_gradients2(self):
+        with tf.GradientTape() as tape:
+            tape.watch([self.__U1, self.__U2])
+            L = self.__get_loss2()
+        g = tape.gradient(L, [self.__U1, self.__U2])
+        return g
 
     @Model._init_model_in_init_model_fit
     @Model._verify_parameters(rules_dict=init_model_verification)
     def initialize_model(self,
-                         batch_size: int = 30,
-                         lmbd1: float = 1e-1,
-                         lmbd2: float = 1e-2,
-                         lr1: float = 1e-1,
-                         lr2: float = 1e-2):
+                         optimizer1="adam",
+                         optimizer2="adam",
+                         lmbd1=0.01,
+                         lmbd2=0.01,
+                         lr1=0.01,
+                         lr2=0.1):
         """
         LINE - initialize_model (step III) \n
         Sets the learning rate and regularization parameters values.
 
-        @param batch_size: Size of the batch passed during fitting.
         @param lmbd1: Regularization parameter for the first loss function.
         @param lmbd2: Regularization parameter for the second loss function.
         @param lr1: Learning rate for the the first loss function optimization.
         @param lr2: Learning rate for the the second loss function optimization.
+        @param optimizer1: Name of the optimizer for the first loss function. Must be one of keras optimizers.
+        @param optimizer1: Name of the optimizer for the second loss function. Must be one of keras optimizers.
         """
-        self.__batch_size = batch_size
         self.__lmbd1 = lmbd1
         self.__lmbd2 = lmbd2
         self.__lr1 = lr1
         self.__lr2 = lr2
+        self.__opt1 = tf.keras.optimizers.get({"class_name": optimizer1, "config": {"learning_rate": lr1}})
+        self.__opt2 = tf.keras.optimizers.get({"class_name": optimizer2, "config": {"learning_rate": lr2}})
 
     @Model._fit_in_init_model_fit
     @Model._verify_parameters(rules_dict=fit_verification)
     def fit(self,
-            num_iter: int = 400,
+            num_iter: int = 300,
             verbose: bool = True):
         """
         LINE - fit (step IV) \n
@@ -119,27 +125,13 @@ class LINE(Model):
         @param num_iter: The number of iterations of the fitting process.
         @param verbose: Verbosity parameter.
         """
-        graph = self.get_graph()
-        for it in range(num_iter):
-            resampled_edges = np.copy(graph.edges)
-            np.random.shuffle(resampled_edges)
-            mini_batches = [resampled_edges[i * self.__batch_size:min(self.__E, i + 1 * self.__batch_size)] for i in
-                            range(self.__E // self.__batch_size)]
-            for batch in mini_batches:
-                step1 = np.zeros(self.__U1.shape)
-                step2 = np.zeros(self.__U2.shape)
-                for (i, j) in batch:
-                    step1[i, :] += self.__grad1(i, j) + self.__lmbd1 * self.__U1[i, :]
-                    step2[i, :] += self.__grad2(i, j) + self.__lmbd2 * self.__U2[i, :]
-                self.__U1 -= self.__lr1 * step1
-                self.__U2 -= self.__lr2 * step2
+        for i in range(num_iter):
+            g1 = self.__get_gradients1()
+            self.__opt1.apply_gradients(zip(g1, [self.__U1, self.__U2]))
+            g2 = self.__get_gradients1()
+            self.__opt1.apply_gradients(zip(g2, [self.__U1, self.__U2]))
             if verbose:
-                print(f"Epoch: {it + 1}, "
-                      f"1st-order objective: {self.__o1(graph):.2f}, "
-                      f"1st-order Frob: {self.__Frob(self.__U1):.2f}, "
-                      f"2nd-order objective: {self.__o2(graph):.2f}, "
-                      f"2nd-order Frob:{self.__Frob(self.__U2):.2f}",
-                      end="\r")
+                print("Epoch " + str(i + 1) + ": loss 1: " + str(self.__get_loss1().numpy()) + ", loss 2:" + str(self.__get_loss2().numpy()))
 
     @Model._embed_in_init_model_fit
     @Model._verify_parameters(rules_dict={})
@@ -149,45 +141,44 @@ class LINE(Model):
         Returns the embedding as a concatenation of two matrices minimizing the two loss functions.
         @return: The embedding matrix having shape Nx(2*d).
         """
-        return np.c_[self.__U1, self.__U2]
+        return np.concatenate((self.__U1.numpy(), self.__U2.numpy()), axis=1)
 
     @staticmethod
     @Model._verify_parameters(rules_dict=fast_embed_verification)
     def fast_embed(graph: Graph,
                    d: int = 2,
-                   batch_size: int = 30,
-                   lmbd1: float = 1e-1,
-                   lmbd2: float = 1e-2,
-                   lr1: float = 1e-1,
-                   lr2: float = 1e-2,
-                   num_iter: int = 400,
-                   fit_verbose: bool = True,
-                   **kwargs):
+                   optimizer1: str = "adam",
+                   optimizer2: str = "adam",
+                   lmbd1: float = 0.01,
+                   lmbd2: float = 0.01,
+                   lr1: float = 0.01,
+                   lr2: float = 0.1,
+                   num_iter: int = 300,
+                   fit_verbose: bool = True):
         """
         LINE - fast_embed \n
         Returns the embedding in a single step.
 
         @param graph: The graph to be embedded. Present in '__init__'
         @param d: The embedding dimension. Present in 'initialize'
-        @param batch_size: Size of the batch passed during fitting. Present in 'initialize_model'
+        @param optimizer1: Name of the optimizer for the first loss function. Must be one of keras optimizers. Present in 'initialize_model'
+        @param optimizer1: Name of the optimizer for the second loss function. Must be one of keras optimizers. Present in 'initialize_model'
         @param lmbd1: Regularization parameter for the first loss function. Present in 'initialize_model'
         @param lmbd2: Regularization parameter for the second loss function. Present in 'initialize_model'
         @param lr1: Learning rate for the the first loss function optimization. Present in 'initialize_model'
         @param lr2: Learning rate for the the second loss function optimization. Present in 'initialize_model'
         @param num_iter: The number of iterations of the fitting process. Present in 'fit'
         @param fit_verbose: Verbosity parameter. Present in 'fit'
-        @param kwargs: Two input matrices 'U1' and 'U2' both of dimension Nxd may be passed here. They are generated
-        from the uniform distribution on [0.0, 1.0) otherwise. Present in 'initialize'
-        @return:
+        @return: The embedding matrix having shape Nx(2*d).
         """
         line = LINE(graph)
-        line.initialize(d,
-                        **kwargs)
-        line.initialize_model(batch_size=batch_size,
-                              lmbd1=lmbd1,
-                              lmbd2=lmbd2,
-                              lr1=lr1,
-                              lr2=lr2)
-        line.fit(num_iter=num_iter,
-                 verbose=fit_verbose)
+        line.initialize(d)
+        line.initialize_model(optimizer1,
+                              optimizer2,
+                              lmbd1,
+                              lmbd2,
+                              lr1,
+                              lr2)
+        line.fit(num_iter,
+                 fit_verbose)
         return line.embed()
